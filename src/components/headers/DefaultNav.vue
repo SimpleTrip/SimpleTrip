2825f95fac35dad880a60eae207a216d3f5dc7ac
김수린 <uhyeon1009@gmail.com>
2023-11-22 09:24:44 +0900

    fix: nav conflict 해결

--- src/components/headers/DefaultNav.vue
+++ src/components/headers/DefaultNav.vue
@@ -29,15 +29,11 @@ const clickLogout = () => {
 </script>
 
 <template>
-  <nav
-    class="navbar navbar-expand-lg top-0 my-3 blur border-radius-lg z-index-3 py-2 shadow py-2 start-0 end-0 mx-4 position-absolute mt-4">
+  <nav class="navbar navbar-expand-lg top-0 my-3 blur border-radius-lg z-index-3 py-2 shadow py-2 start-0 end-0 mx-4 position-absolute mt-4">
     <div class="container-fluid px-0">
-      <router-link class="navbar-brand d-none d-md-block" :to="{ name: 'main' }" style="font-weight: bold">SIMPLE
-        TRIP</router-link>
-      <router-link class="navbar-brand d-block d-md-none" :to="{ name: 'main' }" style="font-weight: bold">SIMPLE
-        TRIP</router-link>
-      <button class="navbar-toggler shadow-none ms-2" data-bs-toggle="collapse" data-bs-target="#navCollapse"
-        aria-controls="navCollapse" aria-expanded="false">
+      <router-link class="navbar-brand d-none d-md-block" :to="{ name: 'main' }" style="font-weight: bold">SIMPLE TRIP</router-link>
+      <router-link class="navbar-brand d-block d-md-none" :to="{ name: 'main' }" style="font-weight: bold">SIMPLE TRIP</router-link>
+      <button class="navbar-toggler shadow-none ms-2" data-bs-toggle="collapse" data-bs-target="#navCollapse" aria-controls="navCollapse" aria-expanded="false">
         <span class="navbar-toggler-icon mt-2">
           <span class="navbar-toggler-bar bar1"></span>
           <span class="navbar-toggler-bar bar2"></span>
@@ -46,16 +42,22 @@ const clickLogout = () => {
       </button>
       <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navCollapse">
         <ul class="navbar-nav navbar-nav-hover ms-auto">
-          <li class="nav-item">
-            <a v-if="isLogin" class="nav-link" @click="clickLogout" href="javascript:">로그아웃</a>
-            <router-link v-else class="nav-link" :to="{ name: 'login' }">로그인</router-link>
+          <template v-if="isLogin">
+            <li class="nav-item">
+              <a v-if="isLogin" class="nav-link" @click="clickLogout">로그아웃</a>
+            </li>
+            <li class="nav-item">
+              <router-link v-if="isLogin" class="nav-link" :to="{ name: 'info' }">회원정보</router-link>
+            </li>
+          </template>
+          <li v-else class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'login' }">로그인</router-link>
           </li>
           <li>
             <router-link class="nav-link" :to="{ name: 'articles' }">게시판</router-link>
           </li>
           <li class="nav-item dropdown dropdown-hover mx-2">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown"
-              aria-expanded="false">여행</a>
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">여행</a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
               <li>
                 <router-link class="dropdown-item" :to="{ name: 'spotScan' }">여행지 찾기</router-link>
@@ -64,8 +66,7 @@ const clickLogout = () => {
             </ul>
           </li>
           <li class="nav-item dropdown dropdown-hover mx-2">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown"
-              aria-expanded="false">장소</a>
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">장소</a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
               <li>
                 <router-link class="dropdown-item" :to="{ name: 'placeRegister' }">장소 등록</router-link>
@@ -79,6 +80,7 @@ const clickLogout = () => {
         </ul>
       </div>
     </div>
-</nav></template>
+  </nav>
+</template>
 
 <style scoped></style>
